495d6deaeec41e8f4934146f03585de893415b0a
alfredo <alfredofredi1709@gmail.com>
2022-06-18 16:18:34 -0500

    Remove last n characters of string

--- challenger.js
+++ challenger.js
@@ -1,13 +1,23 @@
-/*Extract first half of string*/
+/*Remove last n characters of string*/
 
 // Write a function that takes a string (a) as argument
-// Extract the first half a
+// Remove the last 3 characters of a
 // Return the result
 
-function myFunction (a) {
-    return a.slice(0,a.length/2);
-}
 
-console.log(myFunction('abcdefgh'));
+// Your solution
+function myFunction(a){
+    letx=Array.from(a);
+    for(leti=0;i<3;i++)
+    {x.pop();}
+    return x.join('');
+ }
+
+//  The author's solution
+ function myFunction(a){
+    return a.slice(0,-3);
+ }
+
+console.log(myFunction('abcdefg'));
 console.log(myFunction('1234'));
-console.log(myFunction('gedcba'));
+console.log(myFunction('fgedcba'));
