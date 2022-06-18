bda66a6bbadb86c491b4c071f8b3d645b6797a5c
alfredo <alfredofredi1709@gmail.com>
2022-06-18 13:46:19 -0500

    Get first n characters of string

--- challenger.js
+++ challenger.js
@@ -1,11 +1,11 @@
-/*Get last n characters of string*/
+/*Get first n characters of string*/
 
-// Write a function that takes a string as argument
-// Extract the last 3 characters from the string
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
 // Return the result
 
-function myFunction (str) {
-    return str.slice(-3);
+function myFunction (a) {
+    return a.slice(0,3);
 }
 
 console.log(myFunction('abcdefg'));
