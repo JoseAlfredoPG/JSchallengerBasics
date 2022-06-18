6293d073ee0b5d1f247af7a5ee4fff89c9e34194
alfredo <alfredofredi1709@gmail.com>
2022-06-18 13:32:29 -0500

    Comparison operators, strict equality

--- challenger.js
+++ challenger.js
@@ -1,14 +1,14 @@
-/*Sum two numbers*/
+/*Comparison operators, strict equality*/
 
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
 
 
 function myFunction (a,b) {
-    return a+b;
+    return a===b;
 }
 
-console.log(myFunction(1,1));
-console.log(myFunction(1,10));
-console.log(myFunction(99,1));
+console.log(myFunction(2,3));
+console.log(myFunction(3,3));
+console.log(myFunction(1,'1'));
+console.log(myFunction('10','10'));
