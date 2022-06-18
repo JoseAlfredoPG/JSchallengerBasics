e34dcd20b4f9be1ef559ab8583ef08c86b9ca37f
alfredo <alfredofredi1709@gmail.com>
2022-06-18 16:24:43 -0500

    Return the percentage of a number

--- challenger.js
+++ challenger.js
@@ -1,23 +1,21 @@
-/*Remove last n characters of string*/
+/*Return the percentage of a number*/
 
-// Write a function that takes a string (a) as argument
-// Remove the last 3 characters of a
-// Return the result
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
 
 
 // Your solution
-function myFunction(a){
-    letx=Array.from(a);
-    for(leti=0;i<3;i++)
-    {x.pop();}
-    return x.join('');
+function myFunction(a,b){
+    return a*b/100;
  }
 
 //  The author's solution
- function myFunction(a){
-    return a.slice(0,-3);
+ function myFunction(a,b){
+   return b/100*a
  }
 
-console.log(myFunction('abcdefg'));
-console.log(myFunction('1234'));
-console.log(myFunction('fgedcba'));
+
+
+console.log(myFunction(100,50));
+console.log(myFunction(10,1));
+console.log(myFunction(500,25));
