71876d995ddeb4f64d437349dd5df36b06bdf984
alfredo <alfredofredi1709@gmail.com>
2022-06-18 22:28:11 -0500

    Check if a number is a whole number

--- challenger.js
+++ challenger.js
@@ -1,29 +1,22 @@
-/*How many times does a character occur?*/
+/*Check if a number is a whole number*/
 
-// Write a function that takes two strings (a and b) as arguments
-// Return the number of times a occurs in b
+// Write a function that takes a number (a) as argument
+// If a is a whole number (has no decimal place), return true
+// Otherwise, return false
 
 
 // Your solution
-function myFunction(a,b){
-    let convertido=Array.from(b);
-    let contador=0;
-    convertido.map(e=>{
-       if(e===a){contador++;}
-    })
-    return contador;
+function myFunction(a){
+   
+    return Number.isInteger(a);
  }
 
-//    The author's solution
-// info: el metodo esplit divide una cadena y toma como pivote de particion el argumento pasado como parametro.
- function myFunction(a,b){
-  return b.split(a).length-1
-}
-                                    
+//  The author's solution
+ function myFunction(a) {
+ return a - Math.floor (a) === 0;
+ }
 
-
-
-console.log(myFunction('m', 'how many times does the character occur in this sentence?'));
-console.log(myFunction('h', 'how many times does the character occur in this sentence?'));
-console.log(myFunction('?', 'how many times does the character occur in this sentence?'));
-console.log(myFunction('z', 'how many times does the character occur in this sentence?'));
+console.log(myFunction(4));
+console.log(myFunction(1.123));
+console.log(myFunction(1048));
+console.log(myFunction(10.48));
