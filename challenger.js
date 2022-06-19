01fc0aff6921cf0f96de853f4924ccbebd97ec30
alfredo <alfredofredi1709@gmail.com>
2022-06-19 14:34:39 -0500

    Insert character after every n characters (backwards)

--- challenger.js
+++ challenger.js
@@ -1,30 +1,38 @@
-/*Find next higher natural number that is divisble by y*/
+/*Insert character after every n characters (backwards)*/
 
-// Write a function that takes two numbers, say x and y, as arguments
-// Check if x is divisible by y
-// If yes, return x
-// If not, return the next higher natural number that is divisible by y
-
+// Write a function that takes two strings (a and b) as arguments
+// Beginning at the end of 'a', insert 'b' after every 3rd character of 'a'
+// Return the resulting string
 
 // Your solution
-function myFunction(x,y) {
-   if(x%y !== 0) {
-      while(x%y!==0) {
-        x++;
-      }
-      return x;
-   }
-    return x;
+function myFunction(a, b) {
+  let final = [];
+  let ca = Array.from(a);
+  let con = 1;
+  for (let i = ca.length - 1; i >= 0; i--) {
+    final.push(ca[i]);
+    con++;
+    if(con>3)
+    {
+      final.push(b);
+      con=1;
+    }
+  }
+  return final.reverse().join('');
 }
 
 // The author's solution
-
-function myFunction(x, y) {
-  while (x % y !== 0) x++;
-  return x;
+function myFunction(a, b) {
+   let result = [];
+   let rest = a;
+   while (rest.length) {
+   	result.push(rest.slice(-3));
+   	rest = rest.slice(0, -3);
+   }
+   return result.reverse().join(b);
 }
 
-console.log(myFunction(1,23));
-console.log(myFunction(23,23));
-console.log(myFunction(7,3));
-console.log(myFunction(-5,7));
+
+console.log(myFunction('1234567', '.'));
+console.log(myFunction('abcde', '$'));
+console.log(myFunction('zxyzxyzxyzxyzxyz', 'w'));
