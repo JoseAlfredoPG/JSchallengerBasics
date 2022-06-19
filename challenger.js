3d30b52276b1b1492419179ffb8ecf5ca830ba61
alfredo <alfredofredi1709@gmail.com>
2022-06-18 22:12:14 -0500

    Check if a number is even

--- challenger.js
+++ challenger.js
@@ -1,18 +1,16 @@
-/*Basic JavaScript math operators*/
+/*Check if a number is even*/
 
-// Write a function that takes 6 values (a,b,c,d,e,f) as arguments
-// Sum a and b
-// Then substract by c
-// Then multiply by d and divide by e
-// Finally raise to the power of f and return the result
-// Tipp: mind the order
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
 
 
 
-function myFunction(a,b,c,d,e,f){
-    return (((a+b)-c)*d/e)**f;
+function myFunction(a){
+    return a%2===0;
  }
 
-console.log(myFunction(6,5,4,3,2,1));
-console.log(myFunction(6,2,1,4,2,3));
-console.log(myFunction(2,3,6,4,2,3));
+console.log(myFunction(10));
+console.log(myFunction(-4));
+console.log(myFunction(5));
+console.log(myFunction(-111));
