bc0f5c961e57529e70053d81a66f06492046e9c3
alfredo <alfredofredi1709@gmail.com>
2022-06-18 23:09:47 -0500

    Split a number into its digits

--- challenger.js
+++ challenger.js
@@ -1,18 +1,24 @@
-/*Round a number to 2 decimal places*/
+/*Split a number into its digits*/
 
 // Write a function that takes a number (a) as argument
-// Round a to the 2nd digit after the comma
-// Return the rounded number
+// Split a into its individual digits and return them in an array
+// Tipp: you might want to change the type of the number for the splitting
 
 
 // Your solution
 function myFunction(a) {
-  
-    return Number(a.toFixed(2)) ;
+  let x=Array.from(a.toString());
+  return x.map(e=>Number(e));
 }
 
+// The author's solution
 
-console.log(myFunction(2.12397));
-console.log(myFunction(3.136));
-console.log(myFunction(1.12397));
-console.log(myFunction(26.1379));
+function myFunction( a ) {
+  const string = a + '';
+  const strings = string.split('');
+  return strings.map(digit => Number(digit))
+}
+
+console.log(myFunction(10));
+console.log(myFunction(931));
+console.log(myFunction(193278));
