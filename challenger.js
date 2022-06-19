c914076d1233aa3355663811985e4f32d1979fb4
alfredo <alfredofredi1709@gmail.com>
2022-06-19 13:13:07 -0500

    Clear up the chaos behind these strings

--- challenger.js
+++ challenger.js
@@ -1,24 +1,28 @@
 /*Split a number into its digits*/
 
-// Write a function that takes a number (a) as argument
-// Split a into its individual digits and return them in an array
-// Tipp: you might want to change the type of the number for the splitting
+// It seems like something happened to these strings
+// Can you figure out how to clear up the chaos?
+// Write a function that joins these strings together such that they form the following words:
+// 'Javascript', 'Countryside', and 'Downtown'
+// You might want to apply basic JS string methods such as replace(), split(), slice() etc
 
 
 // Your solution
-function myFunction(a) {
-  let x=Array.from(a.toString());
-  return x.map(e=>Number(e));
+function myFunction(a,b) {
+let an=a.replace(a[0],a[0].toUpperCase());
+    let as = an.split('%').join('');
+    let bs = b.split('%').join('').split('').reverse().join('');
+return as+bs;
 }
 
 // The author's solution
-
-function myFunction( a ) {
-  const string = a + '';
-  const strings = string.split('');
-  return strings.map(digit => Number(digit))
+function myFunction(a, b) {
+  const func = x => x.replace('%','');
+  const first = func(a);
+  const second = func(b).split('').reverse().join('');
+  return first.charAt(0).toUpperCase() + first.slice(1) + second;
 }
 
-console.log(myFunction(10));
-console.log(myFunction(931));
-console.log(myFunction(193278));
+console.log(myFunction('java', 'tpi%rcs'));
+console.log(myFunction('c%ountry', 'edis'));
+console.log(myFunction('down', 'nw%ot'));
