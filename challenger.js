ab8ec3bad074417ba44db0b11b2e5161ce4af13b
alfredo <alfredofredi1709@gmail.com>
2022-06-18 22:23:02 -0500

    How many times does a character occur?

--- challenger.js
+++ challenger.js
@@ -1,16 +1,29 @@
-/*Check if a number is even*/
+/*How many times does a character occur?*/
 
-// Write a function that takes a number as argument
-// If the number is even, return true
-// Otherwise, return false
-
+// Write a function that takes two strings (a and b) as arguments
+// Return the number of times a occurs in b
 
 
-function myFunction(a){
-    return a%2===0;
+// Your solution
+function myFunction(a,b){
+    let convertido=Array.from(b);
+    let contador=0;
+    convertido.map(e=>{
+       if(e===a){contador++;}
+    })
+    return contador;
  }
 
-console.log(myFunction(10));
-console.log(myFunction(-4));
-console.log(myFunction(5));
-console.log(myFunction(-111));
+//    The author's solution
+// info: el metodo esplit divide una cadena y toma como pivote de particion el argumento pasado como parametro.
+ function myFunction(a,b){
+  return b.split(a).length-1
+}
+                                    
+
+
+
+console.log(myFunction('m', 'how many times does the character occur in this sentence?'));
+console.log(myFunction('h', 'how many times does the character occur in this sentence?'));
+console.log(myFunction('?', 'how many times does the character occur in this sentence?'));
+console.log(myFunction('z', 'how many times does the character occur in this sentence?'));
