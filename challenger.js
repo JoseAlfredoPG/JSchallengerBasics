2b38f62d1d77e985f767672f897ed6eae8761238
alfredo <alfredofredi1709@gmail.com>
2022-06-18 22:44:52 -0500

    Check whether a string contains another string and concatenate

--- challenger.js
+++ challenger.js
@@ -1,18 +1,26 @@
-/*CMultiplication, division, and comparison operators*/
+/*Check whether a string contains another string and concatenate*/
 
-// Write a function that takes two numbers (a and b) as arguments
-// If a is smaller than b, divide a by b
-// Otherwise, multiply both numbers
-// Return the resulting value
+// Write a function that takes two strings (a and b) as arguments
+// If a contains b, append b to the beginning of a
+// If not, append it to the end
+// Return the concatenation
 
 
 // Your solution
-function myFunction(a,b){
-   
-    return a<b ? a/b : a*b;
- }
+function myFunction(a, b) {
+    if (a.indexOf(b) > 0) {
+        return b + a;
+    }
+    return a + b;
+}
 
-console.log(myFunction(10,100));
-console.log(myFunction(90,45));
-console.log(myFunction(8,20));
-console.log(myFunction(2,0.5));
+//  The author's solution
+
+function myFunction(a, b) {
+    return a.indexOf(b) === -1 ? a + b : b + a
+}
+
+console.log(myFunction('cheese', 'cake'));
+console.log(myFunction('lips', 's'));
+console.log(myFunction('Java', 'script'));
+console.log(myFunction(' think, therefore I am', 'I'));
