57f53817c7068d9ec2820a501b880cef22c8bb83
alfredo <alfredofredi1709@gmail.com>
2022-06-19 13:42:11 -0500

    Return the next higher prime number

--- challenger.js
+++ challenger.js
@@ -1,28 +1,47 @@
-/*Split a number into its digits*/
+/*Return the next higher prime number*/
 
-// It seems like something happened to these strings
-// Can you figure out how to clear up the chaos?
-// Write a function that joins these strings together such that they form the following words:
-// 'Javascript', 'Countryside', and 'Downtown'
-// You might want to apply basic JS string methods such as replace(), split(), slice() etc
+// This challenge is a little bit more complex
+// Write a function that takes a number (a) as argument
+// If a is prime, return a
+// If not, return the next higher prime number
 
 
 // Your solution
-function myFunction(a,b) {
-let an=a.replace(a[0],a[0].toUpperCase());
-    let as = an.split('%').join('');
-    let bs = b.split('%').join('').split('').reverse().join('');
-return as+bs;
+function myFunction(a) {
+    let primo = true;
+    for (let i = 2; i < a / 2; i++) {
+        if (a % i == 0) { primo = false }
+    }
+    if(primo==false) 
+    { 
+       
+        while(primo==false) {
+            a++;
+            primo=true;
+            for (let i = 2; i < a / 2; i++) {
+                if (a % i == 0) { primo = false }
+            }
+        };
+        return a;
+    }
+    return a;
 }
 
 // The author's solution
-function myFunction(a, b) {
-  const func = x => x.replace('%','');
-  const first = func(a);
-  const second = func(b).split('').reverse().join('');
-  return first.charAt(0).toUpperCase() + first.slice(1) + second;
+
+function myFunction( a ) {
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  }
+ let n = a;
+ while (!isPrime(n)) n++;
+return n
 }
 
-console.log(myFunction('java', 'tpi%rcs'));
-console.log(myFunction('c%ountry', 'edis'));
-console.log(myFunction('down', 'nw%ot'));
+console.log(myFunction(38));
+console.log(myFunction(7));
+console.log(myFunction(115));
+console.log(myFunction(2000));
